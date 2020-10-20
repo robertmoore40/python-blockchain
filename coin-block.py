1dc2956e1492ee569315ae0d57554c8b59e58bdc
robertmoore40 <robertcmoore92@gmail.com>
2020-10-20 19:42:45 -0400

    added hash block def

--- coin-block.py
+++ coin-block.py
@@ -8,6 +8,10 @@ class Block:
         self.previous_hash = previous_hash
         self.hash = self.hash_block()
 
-        def hash_block(self):
-            sha = hasher.sha256()
-            sha.update(str(self.index)) +  
+    def hash_block(self):
+        sha = hasher.sha256()
+        sha.update(str(self.index) +
+            str(self.timestamp) + 
+            str(self.data) + 
+            str(self.previous_hash))
+        return sha.hexdigest()
