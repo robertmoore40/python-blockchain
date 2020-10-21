129ece0225cfc6a9082e536aea8e4409f8afddfe
robertmoore40 <robertcmoore92@gmail.com>
2020-10-21 09:36:17 -0400

    added next block def

--- robcoin-genesis.py
+++ robcoin-genesis.py
@@ -1,2 +1,5 @@
 import datetime as date
 
+def create_genesis_block():
+
+    return Block(0, date.datetime.now(), "Genesis Block", "0")
---
+++ robcoin-new-block.py
@@ -0,0 +1,3 @@
+def next_block(last_block):
+    this_index = last_block.index + 1
+    this_timestamp = date.datetime.now()
