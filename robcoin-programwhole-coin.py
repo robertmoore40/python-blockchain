416b259a01050fcde268da1c2bd446da668cd90e
robertmoore40 <robertcmoore92@gmail.com>
2020-10-22 09:09:41 -0400

    added class object data sets

--- robcoin-programwhole-coin.py
+++ robcoin-programwhole-coin.py
@@ -3,3 +3,8 @@ import datetime as date
 
 class Block:
   def __init__(self, index, timestamp, data, previous_hash):
+    self.index = index
+    self.timestamp = timestamp
+    self.data = data
+    self.previous_hash = previous_hash
+    self.hash = self.hash_block()
