36f63805987b138d6728461a56faa8dd5148e848
robertmoore40 <robertcmoore92@gmail.com>
2020-10-27 09:33:13 -0400

    date and timestamp added

--- robcoin-programwhole-coin.py
+++ robcoin-programwhole-coin.py
@@ -21,3 +21,5 @@ def create_genesis_block():
   def next_block(last_block):
   this_index = last_block.index + 1
   this_timestamp = date.datetime.now()
+    this_data = "Hey! I'm block " + str(this_index)
+  this_hash = last_block.hash
